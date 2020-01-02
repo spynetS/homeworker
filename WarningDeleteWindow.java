74d305e8966bf101de147786404aa4c6267f1aff
Alfred <54032594+spynetS@users.noreply.github.com>
2020-01-02 13:26:47 +0100

    Update WarningDeleteWindow.java

--- WarningDeleteWindow.java
+++ WarningDeleteWindow.java
@@ -1,23 +1,36 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.File;
 
-public class WarningDeleteWindow extends JFrame{
+public class WarningDeleteWindow {
     private JButton canselButton;
     private JButton yesButton;
     private JPanel rootName;
     private JButton noButton1;
+    JFrame frame = new JFrame();
+
+    public void Start() {
+        frame.setVisible(true);
+    }
 
     public WarningDeleteWindow(String deleteName)
     {
-        add(rootName);
-        setSize(300,200);
+        frame.add(rootName);
+        frame.setSize(300,200);
+
+canselButton.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    }
+});
 
         noButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
             }
         });
 
@@ -27,6 +40,8 @@ public class WarningDeleteWindow extends JFrame{
                 String[] name = deleteName.split(" ");
                 File file = new File("Files/Vocabularys/"+name[0]+","+name[1]+","+name[2]);
                 file.delete();
+                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+
             }
         });
 
